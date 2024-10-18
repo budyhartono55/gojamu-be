418ce2e99e25aac8d53e7e30ca12d8e52487396c
bhartono.dev <bhartono.dev@sumbawabaratkab.go.id>
2024-10-18 15:50:22 +0800

    filtering service cleared

--- app/Repositories/Service/ServiceRepository.php
+++ app/Repositories/Service/ServiceRepository.php
@@ -43,7 +43,7 @@ class ServiceRepository implements ServiceInterface
     {
         $limit = Helper::limitDatas($request);
         $getSlug = $request->slug;
-        $getCategory = $request->ctg_slg;
+        $getCategory = $request->ctg;
         $getKeyword =  $request->search;
 
         if (!empty($getCategory)) {
@@ -117,8 +117,8 @@ class ServiceRepository implements ServiceInterface
             $service = Service::with(['createdBy', 'editedBy', 'ctgServices'])
                 ->where('ctg_service_id', $category->id)
                 ->where(function ($query) use ($keyword) {
-                    $query->where('title_service', 'LIKE', '%' . $keyword . '%')
-                        ->orWhere('description', 'LIKE', '%' . $keyword . '%');
+                    $query->where('title_service', 'LIKE', '%' . $keyword . '%');
+                    // ->orWhere('description', 'LIKE', '%' . $keyword . '%');
                 })
                 ->latest('created_at')
                 ->paginate($limit);
@@ -143,7 +143,7 @@ class ServiceRepository implements ServiceInterface
             }
             return $this->error("Not Found", "Layanan dengan keyword = ($keyword) dalam Kategori ($slug)tidak ditemukan!", 404);
         } catch (\Exception $e) {
-            return $this->error("Internal Server Error", $e->getMessage());
+            return $this->error("Internal Server Error", $e->getMessage(), 499);
         }
     }
 
@@ -203,8 +203,8 @@ class ServiceRepository implements ServiceInterface
 
             $service = Service::with(['createdBy', 'editedBy', 'ctgServices'])
                 ->where(function ($query) use ($keyword) {
-                    $query->where('title_service', 'LIKE', '%' . $keyword . '%')
-                        ->orWhere('description', 'LIKE', '%' . $keyword . '%');
+                    $query->where('title_service', 'LIKE', '%' . $keyword . '%');
+                    // ->orWhere('description', 'LIKE', '%' . $keyword . '%');
                 })
                 ->latest('created_at')
                 ->paginate($limit);
@@ -233,41 +233,41 @@ class ServiceRepository implements ServiceInterface
         }
     }
 
-    public function showBySlug($slug)
-    {
-        try {
-            $key = $this->generalRedisKeys . "public_" . $slug;
-            $keyAuth = $this->generalRedisKeys . "auth_" . $slug;
-            $key = Auth::check() ? $keyAuth : $key;
-            if (Redis::exists($key)) {
-                $result = json_decode(Redis::get($key));
-                return $this->success("(CACHE): Detail Layanan dengan slug = ($slug)", $result);
-            }
+    // public function showBySlug($slug)
+    // {
+    //     try {
+    //         $key = $this->generalRedisKeys . "public_" . $slug;
+    //         $keyAuth = $this->generalRedisKeys . "auth_" . $slug;
+    //         $key = Auth::check() ? $keyAuth : $key;
+    //         if (Redis::exists($key)) {
+    //             $result = json_decode(Redis::get($key));
+    //             return $this->success("(CACHE): Detail Layanan dengan slug = ($slug)", $result);
+    //         }
 
-            $slug = Str::slug($slug);
-            $service = Service::where('slug', $slug)
-                ->latest('created_at')
-                ->first();
+    //         $slug = Str::slug($slug);
+    //         $service = Service::where('slug', $slug)
+    //             ->latest('created_at')
+    //             ->first();
 
-            if ($service) {
-                $createdBy = User::select('name')->find($service->created_by);
-                $editedBy = User::select('name')->find($service->edited_by);
-                $ctgServices = Ctg_Service::select(['id', 'title_ctg', 'slug'])->find($service->ctg_service_id);
+    //         if ($service) {
+    //             $createdBy = User::select('name')->find($service->created_by);
+    //             $editedBy = User::select('name')->find($service->edited_by);
+    //             $ctgServices = Ctg_Service::select(['id', 'title_ctg', 'slug'])->find($service->ctg_service_id);
 
-                $service->ctg_service_id = optional($ctgServices)->only(['id', 'title_ctg', 'slug']);
-                $service->created_by = optional($createdBy)->only(['name']);
-                $service->edited_by = optional($editedBy)->only(['name']);
+    //             $service->ctg_service_id = optional($ctgServices)->only(['id', 'title_ctg', 'slug']);
+    //             $service->created_by = optional($createdBy)->only(['name']);
+    //             $service->edited_by = optional($editedBy)->only(['name']);
 
-                $key = Auth::check() ? $key : $key;
-                Redis::setex($key, 60, json_encode($service));
-                return $this->success("Detail Layanan dengan slug = ($slug)", $service);
-            } else {
-                return $this->error("Not Found", "Layanan dengan slug = ($slug) tidak ditemukan!", 404);
-            }
-        } catch (\Exception $e) {
-            return $this->error("Internal Server Error", $e->getMessage(), 499);
-        }
-    }
+    //             $key = Auth::check() ? $key : $key;
+    //             Redis::setex($key, 60, json_encode($service));
+    //             return $this->success("Detail Layanan dengan slug = ($slug)", $service);
+    //         } else {
+    //             return $this->error("Not Found", "Layanan dengan slug = ($slug) tidak ditemukan!", 404);
+    //         }
+    //     } catch (\Exception $e) {
+    //         return $this->error("Internal Server Error", $e->getMessage(), 499);
+    //     }
+    // }
 
     // findOne
     public function findById($id)
@@ -292,7 +292,7 @@ class ServiceRepository implements ServiceInterface
                 $service->edited_by = optional($editedBy)->only(['name']);
                 $service->ctg_service_id = optional($ctgService)->only(['id', 'title_ctg', 'slug']);
 
-                $key = Auth::check() ? $key : $key;
+                $key = Auth::check() ? $keyAuth . $id : $key . $id;
                 Redis::setex($key, 60, json_encode($service));
                 return $this->success("Detail Layanan dengan ID = ($id)", $service);
             } else {
